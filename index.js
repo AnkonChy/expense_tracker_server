ebc954a590c460ea5f9521aa7882d2e733fd4f32
Ankon Chowdhury <ankonchy1@gmail.com>
2025-07-05 13:48:01 +0600

    delete api created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ri84s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 app.use(cors());
 app.use(express.json());
@@ -31,6 +31,15 @@ async function run() {
     //all expense
     app.get("/allExpense", async (req, res) => {
       const result = await expenseCollection.find().toArray();
+      res.send(result);
+    });
+
+    //delete expense
+    app.delete("/expense/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await expenseCollection.deleteOne(query);
       res.send(result)
     });
   } finally {
